b5e7c89fef8340e921d468f36f3eb864389fdb24
Renato Mendes <mendeslrenato@gmail.com>
2022-12-02 18:45:26 -0300

    refactor: move FormStatus to its own component

---
+++ src/presentation/components/form-status/form-status.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styles from './form-status-styles.scss'
+import Spinner from '@/presentation/components/spinner/spinner'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+const FormStatus: React.FC<Props> = (props: Props) => {
+  return (
+    <div className={Styles.errorWrap}>
+    <Spinner className={Styles.spinner} />
+    <span className={Styles.error}>Error</span>
+  </div>
+  )
+}
+
+export default FormStatus
