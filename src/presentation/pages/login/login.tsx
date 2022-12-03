36d0c81f4dffccd6cf4e1ec96b30d32aa78122d6
Renato Mendes <mendeslrenato@gmail.com>
2022-12-02 23:03:22 -0300

    feat: ensure Login Page show password status error if Validation fails

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -14,18 +14,16 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     email: '',
     password: '',
     emailError: '',
-    passwordError: 'required field',
+    passwordError: '',
     errorMessage: ''
   })
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email', state.email)
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password)
     })
-  }, [state.email])
-  useEffect(() => {
-    validation.validate('password', state.password)
-  }, [state.password])
+  }, [state.email, state.password])
 
   return (
     <div className={Styles.login}>
