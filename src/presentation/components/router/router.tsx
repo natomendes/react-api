87bb4e12f0c31975439da00d93fd5ffdce4e732f
Renato Mendes <mendeslrenato@gmail.com>
2022-12-03 01:59:28 -0300

    feat: ensure Login Page calls authentication with correct values

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,12 @@
 import { Login } from '@/presentation/pages'
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { ValidationStub } from '../../../../tests/presentation/mocks/mock-validation'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/login' exact render={() => <Login validation={new ValidationStub()}/>} />
+        <Route path='/login' exact component={Login} />
       </Switch>
     </BrowserRouter>
   )
