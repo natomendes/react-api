8d1294c2c47441257ce4410d10ec57f99c105e77
Renato Mendes <mendeslrenato@gmail.com>
2022-12-02 21:34:16 -0300

    refactor: move sut creation to a factory method

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -1,37 +1,50 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import { Login } from '@/presentation/pages'
 
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+  return {
+    sut
+  }
+}
+
 describe('Login Page', () => {
-  it('Should not render Spinner component on start', () => {
-    const { queryByTestId } = render(<Login />)
-    const spinnerComponent = queryByTestId('spinner')
-    expect(spinnerComponent).toBeNull()
-  })
+  describe('Initial State', () => {
+    it('Should not render Spinner component on start', () => {
+      const { sut } = makeSut()
+      const spinnerComponent = sut.queryByTestId('spinner')
+      expect(spinnerComponent).toBeNull()
+    })
 
-  it('Should not render error message span component on start', () => {
-    const { queryByTestId } = render(<Login />)
-    const errorMessageSpan = queryByTestId('error-message-span')
-    expect(errorMessageSpan).toBeNull()
-  })
+    it('Should not render error message span component on start', () => {
+      const { sut } = makeSut()
+      const errorMessageSpan = sut.queryByTestId('error-message-span')
+      expect(errorMessageSpan).toBeNull()
+    })
 
-  it('Should have submit button disable on start', () => {
-    const { getByRole } = render(<Login />)
-    const submitButton = getByRole('button', { name: /enter/i })
-    expect(submitButton).toHaveProperty('disabled', true)
-  })
+    it('Should have submit button disable on start', () => {
+      const { sut } = makeSut()
+      const submitButton = sut.getByRole('button', { name: /enter/i })
+      expect(submitButton).toHaveProperty('disabled', true)
+    })
 
-  it('Should have email status title "required field" and text content "🔴" on start', () => {
-    const { getByTestId } = render(<Login />)
-    const emailStatus = getByTestId('email-status')
-    expect(emailStatus.title).toBe('required field')
-    expect(emailStatus.textContent).toBe('🔴')
-  })
+    it('Should have email status title "required field" and text content "🔴" on start', () => {
+      const { sut } = makeSut()
+      const emailStatus = sut.getByTestId('email-status')
+      expect(emailStatus.title).toBe('required field')
+      expect(emailStatus.textContent).toBe('🔴')
+    })
 
-  it('Should have password status title "required field" and text content "🔴" on start', () => {
-    const { getByTestId } = render(<Login />)
-    const passwordStatus = getByTestId('password-status')
-    expect(passwordStatus.title).toBe('required field')
-    expect(passwordStatus.textContent).toBe('🔴')
+    it('Should have password status title "required field" and text content "🔴" on start', () => {
+      const { sut } = makeSut()
+      const passwordStatus = sut.getByTestId('password-status')
+      expect(passwordStatus.title).toBe('required field')
+      expect(passwordStatus.textContent).toBe('🔴')
+    })
   })
 })
