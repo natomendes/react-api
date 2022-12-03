2419ae657902f2bdcf2502f2b93d8af33b2c1262
Renato Mendes <mendeslrenato@gmail.com>
2022-12-03 01:39:05 -0300

    feat: ensure Login Page shows spinner on submit

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -6,10 +6,10 @@ import Context from '@/presentation/contexts/form/form-context'
 type Props = React.HTMLAttributes<HTMLElement>
 
 const FormStatus: React.FC<Props> = (props: Props) => {
-  const { isLoading, errorMessage } = useContext(Context)
+  const { state: { isLoading, errorMessage } } = useContext(Context)
   return (
     <div className={Styles.errorWrap}>
-    { isLoading && <Spinner className={Styles.spinner} data-testid="spinner"/> }
+    { isLoading && <Spinner className={Styles.spinner} /> }
     { errorMessage && <span className={Styles.error} data-testid="error-message-span">{ errorMessage }</span> }
   </div>
   )
