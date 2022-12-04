c90a4c264e5a4cdb78efdc7c88c715d0cc3f4383
Renato Mendes <mendeslrenato@gmail.com>
2022-12-04 20:36:33 -0300

    chore: add files to coverage ignore

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   // collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts',
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/infra/http/axios-http-client-mock.ts',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
