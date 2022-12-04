e572c855b68a224a7bde94f6693b8286ac45d905
Renato Mendes <mendeslrenato@gmail.com>
2022-12-04 02:54:42 -0300

    test: ensure ValidationBuilder returns a RequiredFieldValidator

---
+++ tests/validation/validation-builder.spec.ts
@@ -0,0 +1,8 @@
+import { RequiredFieldValidation, ValidationBuilder as sut } from '@/validation/validators'
+
+describe('ValidationBuilder', () => {
+  it('Should return RequiredFieldValidation', () => {
+    const validations = sut.field('any_field').required().build()
+    expect(validations).toEqual([new RequiredFieldValidation('any_field')])
+  })
+})
