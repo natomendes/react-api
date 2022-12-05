f7ef0ba71ef5b7603f63bf055a4b329492ef5c33
Renato Mendes <mendeslrenato@gmail.com>
2022-12-05 02:35:11 -0300

    test: ensure RemoteAddAccount throws EmailInUseerror if HttpPostClient returns not found

--- tests/data/usecases/remote-add-account.spec.ts
+++ tests/data/usecases/remote-add-account.spec.ts
@@ -54,6 +54,15 @@ describe('RemoteAddAccount', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
+  it('Should throw UnexpectedError if HttpPostClient returns not found', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
   it('Should throw UnexpectedError if HttpPostClient returns server error', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
