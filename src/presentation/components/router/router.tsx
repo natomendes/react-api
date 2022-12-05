1c7c6ca99da8d4650c15bbedb13b76eee13395af
Renato Mendes <mendeslrenato@gmail.com>
2022-12-05 03:38:51 -0300

    feat: add SignUp view

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { makeLoginPage } from '@/main/factories/pages/login'
+import { SignUp } from '@/presentation/pages'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/login' exact component={makeLoginPage} />
+        <Route path='/signup' exact component={SignUp} />
       </Switch>
     </BrowserRouter>
   )
---
+++ src/presentation/pages/signup/signup.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Styles from './signup-styles.scss'
+import Context from '@/presentation/contexts/form/form-context'
+import { LoginHeader, Footer, Input, FormStatus } from '@/presentation/components'
+
+const SignUp: React.FC = () => {
+  return (
+    <div className={Styles.signup}>
+      <LoginHeader />
+
+      <Context.Provider value={ { state: {} } }>
+        <form
+          className={Styles.form}
+          role="form"
+        >
+          <h2>Create Account</h2>
+
+          <Input
+            type="text"
+            name="name"
+            placeholder='enter your full name'
+          />
+          <Input
+            type="email"
+            name="email"
+            placeholder='enter your email address'
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder='enter your password'
+          />
+          <Input
+            type="password"
+            name="passwordConfirmation"
+            placeholder='confirm your password'
+          />
+
+          <button
+            className={Styles.submit}
+            type="submit"
+          >
+            Create Account
+          </button>
+
+          <Link
+            className={Styles.link}
+            to='/login'
+          >
+            sign in
+          </Link>
+
+          <FormStatus />
+        </form>
+      </Context.Provider>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default SignUp
