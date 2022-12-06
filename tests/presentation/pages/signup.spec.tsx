cec5dcce4696c01f73f0bedc3c4b28f9eeb08c03
Renato Mendes <mendeslrenato@gmail.com>
2022-12-06 01:12:30 -0300

    test: ensure SignUp page shows correct name status if Validation fails

--- tests/presentation/pages/signup.spec.tsx
+++ tests/presentation/pages/signup.spec.tsx
@@ -1,48 +1,59 @@
 import React from 'react'
-import { cleanup, fireEvent, RenderResult } from '@testing-library/react'
-import { MemoryHistory, createMemoryHistory } from 'history'
+import { faker } from '@faker-js/faker'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { SignUp } from '@/presentation/pages'
+import { ValidationStub } from '@/tests/presentation/mocks'
+import { cleanup, fireEvent, RenderResult } from '@testing-library/react'
+import { checkFieldStatus, checkButtonIsDisabled, checkElementNotExists, populateNameField } from '@/tests/presentation/pages/test-helpers'
 import { renderWithRouter } from '@/tests/presentation/pages/renderWithRouter'
-import { checkButtonIsDisabled, checkElementNotExists, checkFieldStatus } from '@/tests/presentation/pages/test-helpers'
 
 type SutTypes = {
   sut: RenderResult
+  validationStub: ValidationStub
   history: MemoryHistory
 }
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string
+}
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationStub = new ValidationStub()
   const history = createMemoryHistory()
+  validationStub.errorMessage = params?.validationError
   const sut = renderWithRouter(
-    <SignUp />,
-    history
+    <SignUp
+      validation={validationStub}
+    />, history
   )
   return {
     sut,
+    validationStub,
     history
   }
 }
 
 describe('SignUp Page', () => {
-  afterEach(cleanup)
   describe('Initial State', () => {
+    afterEach(cleanup)
     it('Should not render Spinner component on start', () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut({ validationError: faker.random.words() })
       checkElementNotExists(sut, 'spinner')
     })
 
     it('Should not render error message span component on start', () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut({ validationError: faker.random.words() })
       checkElementNotExists(sut, 'error-message-span')
     })
 
     it('Should have submit button disable on start', () => {
-      const { sut } = makeSut()
+      const { sut } = makeSut({ validationError: faker.random.words() })
       checkButtonIsDisabled(sut, 'Create Account', true)
     })
 
     it('Should have name status title = "Your name is invalid" and text content "🔴" on start', () => {
-      const { sut } = makeSut()
-      checkFieldStatus(sut, 'name', 'Your name is invalid')
+      const { sut, validationStub } = makeSut({ validationError: faker.random.words() })
+      checkFieldStatus(sut, 'name', validationStub.errorMessage)
     })
 
     it('Should have email status title = "Your email is invalid" and text content "🔴" on start', () => {
@@ -67,11 +78,18 @@ describe('SignUp Page', () => {
     })
   })
   describe('Component Flow', () => {
+    afterEach(cleanup)
     it('Should have inputs as read only false on focus', () => {
       const { sut } = makeSut()
       const emailInput = sut.getByPlaceholderText('enter your email address')
       fireEvent.focus(emailInput)
       expect(emailInput).toHaveProperty('readOnly', false)
+    })
+
+    it('Should show name status error if Validation fails', () => {
+      const { sut, validationStub } = makeSut({ validationError: faker.random.words() })
+      populateNameField(sut)
+      checkFieldStatus(sut, 'name', validationStub.errorMessage)
     })
   })
 })
