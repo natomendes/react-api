0fbb13f85d367b0fa791389c311f4a8ae64d8b4a
Renato Mendes <mendeslrenato@gmail.com>
2022-12-05 23:25:09 -0300

    test: ensure SignUp Page does not render spinner component on start

---
+++ tests/presentation/pages/signup.spec.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { MemoryHistory, createMemoryHistory } from 'history'
+import { SignUp } from '@/presentation/pages'
+import { renderWithRouter } from './renderWithRouter'
+import { RenderResult } from '@testing-library/react'
+import { checkElementNotExists } from './test-helpers'
+
+type SutTypes = {
+  sut: RenderResult
+  history: MemoryHistory
+}
+
+const makeSut = (): SutTypes => {
+  const history = createMemoryHistory()
+  const sut = renderWithRouter(
+    <SignUp />,
+    history
+  )
+  return {
+    sut,
+    history
+  }
+}
+
+describe('SignUp Page', () => {
+  describe('Initial State', () => {
+    it('Should not render Spinner component on start', () => {
+      const { sut } = makeSut()
+      checkElementNotExists(sut, 'spinner')
+    })
+  })
+})
