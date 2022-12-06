2327787a416607acf3d8e71cf9e11680787fb667
Renato Mendes <mendeslrenato@gmail.com>
2022-12-05 23:53:08 -0300

    test: ensure SignUp page have correct email status on start

--- tests/presentation/pages/signup.spec.tsx
+++ tests/presentation/pages/signup.spec.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
+import { RenderResult } from '@testing-library/react'
 import { MemoryHistory, createMemoryHistory } from 'history'
 import { SignUp } from '@/presentation/pages'
-import { renderWithRouter } from './renderWithRouter'
-import { RenderResult } from '@testing-library/react'
-import { checkElementNotExists } from './test-helpers'
+import { renderWithRouter } from '@/tests/presentation/pages/renderWithRouter'
+import { checkButtonIsDisabled, checkElementNotExists, checkFieldStatus } from '@/tests/presentation/pages/test-helpers'
 
 type SutTypes = {
   sut: RenderResult
@@ -32,6 +32,16 @@ describe('SignUp Page', () => {
     it('Should not render error message span component on start', () => {
       const { sut } = makeSut()
       checkElementNotExists(sut, 'error-message-span')
+    })
+
+    it('Should have submit button disable on start', () => {
+      const { sut } = makeSut()
+      checkButtonIsDisabled(sut, 'Create Account', true)
+    })
+
+    it('Should have email status title = "Your email is invalid" and text content "🔴" on start', () => {
+      const { sut } = makeSut()
+      checkFieldStatus(sut, 'email', 'Your email is invalid')
     })
   })
 })
