ffe76f6808d4a4cb70a9fd4ad6bf52430aa564b6
Renato Mendes <mendeslrenato@gmail.com>
2022-12-06 01:17:58 -0300

    feat: ensure SignUp page shows correct email status if Validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -12,8 +12,9 @@ type Props = {
 const SignUp: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     name: '',
+    email: '',
     nameError: '',
-    emailError: 'Your email is invalid',
+    emailError: '',
     passwordError: 'Your password is invalid',
     passwordConfirmationError: 'Your passwordConfirmation is invalid'
   })
@@ -21,9 +22,10 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
   useEffect(() => {
     setState({
       ...state,
-      nameError: validation.validate('name', state.name)
+      nameError: validation.validate('name', state.name),
+      emailError: validation.validate('email', state.email)
     })
-  }, [state.name])
+  }, [state.name, state.email])
   return (
     <div className={Styles.signup}>
       <LoginHeader />
