f87959c40e52e4900b56d0d1c36760dba44e8482
Renato Mendes <mendeslrenato@gmail.com>
2022-12-07 00:27:16 -0300

    feat: add SignUp Composition

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { makeLoginPage } from '@/main/factories/pages/login'
-import { SignUp } from '@/presentation/pages'
+import { makeSignUpPage } from '@/main/factories/pages/signup'
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/login' exact component={makeLoginPage} />
-        <Route path='/signup' exact component={SignUp} />
+        <Route path='/signup' exact component={makeSignUpPage} />
       </Switch>
     </BrowserRouter>
   )
